fe878f379d2c664810fdf7538fed77ebccc33bea
Konstantin Vorobyev <konstantin.vorobev@ic-group.ru>
2025-07-18 13:46:12 +0300

    create second migration

---
+++ TaskManagementActivitiesLogging/src/Persistence/Migrations/20250718103325_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ACTIVITY_LOGS",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    event_type = table.Column<string>(type: "character varying(32)", maxLength: 32, nullable: false),
+                    entity = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    entity_id = table.Column<long>(type: "bigint", nullable: false),
+                    event_time = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ACTIVITY_LOGS", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ACTIVITY_LOGS");
+        }
+    }
+}
